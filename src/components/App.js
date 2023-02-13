953d76d2afd68544f99f32b948e2a483f47c98b6
Shetty02 <shettynehal87@gmail.com>
2023-02-13 18:57:26 +0530

    done changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,90 +1,63 @@
-import React, {useState } from "react";
+import React, {Component, useState } from "react";
+import '../styles/App.css'
 
-
-const relation = (str1, str2) => {
-  let common = "";
-  for (let i = 0; i < str1.length; i++) {
-    if (str2.indexOf(str1[i]) !== -1) {
-      common += str1[i];
-      str2 = str2.replace(str1[i], "");
+function App() {
+    const[str1,setStr1] = useState("");
+    const[str2,setStr2] = useState("");
+    const[isError, setIsError] = useState(false);
+    const[relation,setRealtion] = useState("");
+    
+    const handleStr1 = (e) =>{
+        setStr1(e.target.value);
     }
-  }
-
-  let str1WithoutCommon = "";
-  for (let i = 0; i < str1.length; i++) {
-    if (common.indexOf(str1[i]) === -1) {
-      str1WithoutCommon += str1[i];
+  
+    const handleStr2 = (e) =>{
+        setStr2(e.target.value);
     }
-  }
-
-  let str2WithoutCommon = "";
-  for (let i = 0; i < str2.length; i++) {
-    str2WithoutCommon += str2[i];
-  }
-
-  let totalLength = str1WithoutCommon.length + str2WithoutCommon.length;
-  switch (totalLength % 6) {
-    case 1:
-      return "Friends";
-    case 2:
-      return "Love";
-    case 3:
-      return "Affection";
-    case 4:
-      return "Marriage";
-    case 5:
-      return "Enemy";
-    case 0:
-      return "Siblings";
-    default:
-      return "";
-  }
-};
-
-const App = () => {
-  const [str1, setStr1] = useState("");
-  const [str2, setStr2] = useState("");
-  const [relationType, setRelationType] = useState("");
-
-  const handleStr1Change = (event) => {
-    setStr1(event.target.value);
-  };
-
-  const handleStr2Change = (event) => {
-    setStr2(event.target.value);
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    setRelationType(relation(str1, str2));
-  };
-
-  return (
-    <div id="main">
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>
-            String 1:
-            <input type="text" value={str1} onChange={handleStr1Change} />
-          </label>
-        </div>
-        <div>
-          <label>
-            String 2:
-            <input type="text" value={str2} onChange={handleStr2Change} />
-          </label>
-        </div>
-        <button type="submit">Submit</button>
-      </form>
+  
+    const relationList = {
+      0:"Siblings",
+      1:"Friends",
+      2:"Love",
+      3:"Affection",
+      4:"Marriage",
+      5:"Enemy"
+    }
+    const handelCalculate = () =>{
+      if(!str1 || !str2) setIsError(true);
+      else{
+        setIsError(false)
+        let sum = 0;
+        for(let i = 0; i < str1.length; i++){
+          if(str2.includes(str1[i])){
+            sum = str1.length + str2.length - 2;
+            break;
+          }
+        }
+        let res = sum % 6;
+        setRealtion(relationList[res]);
+      }
+      }
+  
+    const handleClear = () =>{
+      setStr1("");
+      setStr2("");
+      setRealtion("");
+    }
+  
+    let errorMsg = "Please Enter valid input";
+    return (
       <div>
-        {relationType === "" ? (
-          <p>Please Enter valid input</p>
-        ) : (
-          <p>Relation type: {relationType}</p>
-        )}
+        <h1>Flames</h1>
+        <input type="text" value={str1} data-testid="input1" onChange = {handleStr1}/>
+        <br/>
+        Second Name:
+        <input type="text" value={str2} data-testid="input2" onChange = {handleStr2}/>
+        <br/>
+        <button  data-testid="calculate_relationship" onClick={handelCalculate}>Calculate RelationShip</button>
+        <button data-testid="clear" onClick={handleClear}>Clear</button>
+        <h3 data-testid="answer">Result: {isError ? errorMsg: relation}</h3>
       </div>
-    </div>
-  );
-};
-
+    );
+  }
 export default App;
